85a9e4d283de64c2b09129d66cfd671ad548d1f2
Talibat 3ilm <omnia.hefzy@gmail.com>
2023-02-16 21:54:04 +0300

    adding meta tags and basic app structure

---
+++ src/components/Footer.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+function Footer() {
+    return
+        <>
+            <img alt="footer-logo" ></img>
+            <nav>
+                <h1>Doormat Navigation</h1>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Menu</a></li>
+                    <li><a href="#">Reservations</a></li>
+                    <li><a href="#">Order online</a></li>
+                    <li><a href="#">Login</a></li>
+                </ul>
+            </nav>
+            <nav>
+                <h1>Contact</h1>
+                <ul>
+                    <li><a href="#">Address</a></li>
+                    <li><a href="#">phone number</a></li>
+                    <li><a href="#">email</a></li>
+                </ul>
+            </nav>
+            <nav>
+                <h1>Social media links</h1>
+                <ul>
+                    <li><a href="#">Address</a></li>
+                    <li><a href="#">phone number</a></li>
+                    <li><a href="#">email</a></li>
+                </ul>
+            </nav>
+        </>;
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from "../imgicon/icons_assets/logo.svg"
+
+function Header() {
+    return(
+        <>
+            <img src="" alt={Logo} ></img>
+        </>
+    );
+}
+
+export default Header;
